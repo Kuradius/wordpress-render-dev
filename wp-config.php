2b7bf2da80aad30dd1fee4b5e16639e2e8e65fd9
Kuradius <100485274+Kuradius@users.noreply.github.com>
2025-07-19 15:06:29 +0200

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -1,4 +1,6 @@
 <?php
+
+// ** Database instellingen ** //
 define('DB_NAME', 'sql7790806');
 define('DB_USER', 'sql7790806');
 define('DB_PASSWORD', 'kJTavXkJUN');
@@ -6,9 +8,11 @@ define('DB_HOST', 'sql7.freesqldatabase.com:3306');
 define('DB_CHARSET', 'utf8');
 define('DB_COLLATE', '');
 
+// ** Site URL instellingen ** //
 define('WP_HOME', 'https://wordpress-render-dev.onrender.com');
 define('WP_SITEURL', 'https://wordpress-render-dev.onrender.com');
 
+// ** Authentication Unique Keys and Salts. **
 define('AUTH_KEY',         'random-key-hier');
 define('SECURE_AUTH_KEY',  'random-key-hier');
 define('LOGGED_IN_KEY',    'random-key-hier');
@@ -18,10 +22,16 @@ define('SECURE_AUTH_SALT', 'random-key-hier');
 define('LOGGED_IN_SALT',   'random-key-hier');
 define('NONCE_SALT',       'random-key-hier');
 
+// ** Tabel prefix ** //
 $table_prefix = 'wp_';
+
+// ** Debug-modus ** //
 define('WP_DEBUG', false);
 
-if ( !defined('ABSPATH') )
-	define('ABSPATH', dirname(__FILE__) . '/');
+// ** Absolute pad naar WordPress-directory ** //
+if (!defined('ABSPATH')) {
+    define('ABSPATH', dirname(__FILE__) . '/');
+}
 
+// ** Zet WordPress in gang ** //
 require_once(ABSPATH . 'wp-settings.php');
