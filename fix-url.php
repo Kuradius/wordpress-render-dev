dd063bf0a44539538ca41c0cf8aad2c2c4ea1109
Kuradius <100485274+Kuradius@users.noreply.github.com>
2025-07-19 15:13:30 +0200

    Verwijder fix url

--- fix-url.php
+++ fix-url.php
@@ -1,8 +1,1 @@
-<?php
-define('WP_USE_THEMES', false);
-require('./wp-blog-header.php');
 
-update_option('siteurl', 'https://wordpress-render-dev.onrender.com');
-update_option('home', 'https://wordpress-render-dev.onrender.com');
-
-echo "Site URL en Home URL zijn nu correct ingesteld.";
