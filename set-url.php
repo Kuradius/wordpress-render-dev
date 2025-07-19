a795788b57beeacc9fc919b19b0b138c55099953
Kuradius <100485274+Kuradius@users.noreply.github.com>
2025-07-19 15:29:15 +0200

    Create set-url.php

---
+++ set-url.php
@@ -0,0 +1,7 @@
+<?php
+require_once('wp-load.php');
+
+update_option('siteurl', 'https://wordpress-render-dev.onrender.com');
+update_option('home', 'https://wordpress-render-dev.onrender.com');
+
+echo '✅ URLs zijn correct ingesteld. Verwijder dit bestand meteen!';
